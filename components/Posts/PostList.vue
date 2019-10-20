a25df5a3eff1b5678a1032b4494ac4d5be29fd86
a@fandb.pro <a@fandb.pro>
2019-10-20 14:58:29 +0300

    Basic functional is alive and ready to go

---
+++ components/Posts/PostList.vue
@@ -0,0 +1,55 @@
+<template>
+  <section class="post-list">
+    <PostPreview
+      v-for="post in posts"
+      :key="post.id"
+      :id="post.id"
+      :is-admin="isAdmin"
+      :image="post.image"
+      :title="post.title"
+      :slug="post.slug"
+      :category="post.category.name"
+      :description="post.description" />
+  </section>
+</template>
+
+<script>
+import PostPreview from '@/components/Posts/PostPreview'
+
+export default {
+  components: {
+    PostPreview
+  },
+  /*asyncData(context, callback) {
+    setTimeout(() => {
+      callback(null, () => {
+
+      })
+    }, 100)
+  },*/
+  props: {
+    isAdmin: {
+      type: Boolean,
+      default: false
+    },
+    posts: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+
+.post-list {
+  display: flex;
+  padding: 20px;
+  box-sizing: border-box;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+}
+</style>
+
