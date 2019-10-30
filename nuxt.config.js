74db51d8cf9fbef0f1385c8253508f7dc04c71f1
a@fandb.pro <a@fandb.pro>
2019-10-30 22:54:30 +0300

    Meta title added to categories

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,14 +11,14 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'WD Blog',
+    title: 'LOVA News',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       {
         hid: 'description',
         name: 'description',
-        content: 'My cool Web Development Blog'
+        content: 'Fresh news from all over the World.'
       }
     ],
     link: [
--- pages/news/_category/index.vue
+++ pages/news/_category/index.vue
@@ -13,12 +13,12 @@
   export default {
     name: 'index',
     components: { PostList },
-    async asyncData(context) {
-      let category = await context.route.params.category;
-      let getInitialPosts = await context.app.$axios('http://admin.lova.news/news/12/' + category);
+    async asyncData (context) {
+      let category = await context.route.params.category
+      let getInitialPosts = await context.app.$axios('http://admin.lova.news/news/12/' + category)
       let categories = await context.app.$axios('http://admin.lova.news/categories')
-      context.store.commit('setPostsByCategory', []);
-      context.store.commit('setPostsByCategory', getInitialPosts.data.data);
+      context.store.commit('setPostsByCategory', [])
+      context.store.commit('setPostsByCategory', getInitialPosts.data.data)
       context.store.commit('setNextCategoryPage', getInitialPosts.data['next_page_url'])
       context.store.commit('setCategories', categories.data)
       return {
@@ -27,13 +27,25 @@
       }
     },
     methods: {
-      loadNewPosts() {
+      loadNewPosts () {
         this.$axios.get(this.$store.getters.getNextCategoryPage)
           .then(res => {
             this.$store.commit('setPostsByCategory', [...this.$store.getters.getPostsByCategory, ...res.data.data])
-            this.$store.commit('setNextCategoryPage', res.data['next_page_url']);
-            this.categoryPosts = this.$store.getters.getPostsByCategory;
-          });
+            this.$store.commit('setNextCategoryPage', res.data['next_page_url'])
+            this.categoryPosts = this.$store.getters.getPostsByCategory
+          })
+      },
+      firstUpper (value) {
+        if (!value) {
+          return ''
+        }
+        value = value.toString()
+        return value.charAt(0).toUpperCase() + value.slice(1)
+      }
+    },
+    head () {
+      return {
+        title: `${this.firstUpper(this.category)} News`,
       }
     }
   }
