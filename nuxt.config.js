fefcea51b9b16aae5e732231f0ccd080a19ce370
a@fandb.pro <a@fandb.pro>
2019-11-10 09:42:43 +0300

    [v/7 - public path added ''] - changed router position] All working on dev, initial router handling

--- nuxt.config.js
+++ nuxt.config.js
@@ -77,11 +77,7 @@ module.exports = {
   ** Build configuration
   */
   build: {
-    extend (config, { isDev }) {
-      if (!isDev) {
-        config.output.publicPath = './_nuxt/'
-      }
-    },
+    publicPath: '',
     postcss: {
       // Add plugin names as key and arguments as value
       // Install them before as dependencies with npm or yarn
