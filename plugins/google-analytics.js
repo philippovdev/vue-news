0218fa27374d7abf8425b8f4e80b4dc751ac4f32
a@fandb.pro <a@fandb.pro>
2019-11-16 17:05:03 +0300

    deleted context

--- plugins/google-analytics.js
+++ plugins/google-analytics.js
@@ -3,19 +3,14 @@ import vueAnalytics from 'vue-analytics'
 
 const getGDPR = localStorage.getItem('GDPR:accepted')
 
-export default (ctx, inject) => {
-  if (typeof getGDPR !== 'undefined' && getGDPR === 'true') {
-    Vue.use(vueAnalytics, {
-      id: 'UA-74645070-2',
-      disabled: false,
-      route: ctx.app.router
-    })
-    ctx.$ga = Vue.$ga
-    inject('ga', Vue.$ga)
-  } else if (typeof getGDPR !== 'undefined' && getGDPR === 'false') {
-    Vue.use(vueAnalytics, {
-      id: 'UA-74645070-2',
-      disabled: true,
-    })
-  }
+if (typeof getGDPR !== 'undefined' && getGDPR === 'true') {
+  Vue.use(vueAnalytics, {
+    id: 'UA-74645070-2',
+    disabled: false
+  })
+} else if (typeof getGDPR !== 'undefined' && getGDPR === 'false') {
+  Vue.use(vueAnalytics, {
+    id: 'UA-74645070-2',
+    disabled: true,
+  })
 }
