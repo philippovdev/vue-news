c65ee93c122cd708619d43d8836106b15eab2705
a@fandb.pro <a@fandb.pro>
2019-11-16 14:59:33 +0300

    enabled nuxt-analytics

--- plugins/google-analytics.js
+++ plugins/google-analytics.js
@@ -4,11 +4,18 @@ import vueAnalytics from 'vue-analytics'
 const getGDPR = localStorage.getItem('GDPR:accepted')
 
 if (typeof getGDPR !== 'undefined' && getGDPR === 'true') {
+  console.log('I am ebled')
   Vue.use(vueAnalytics, {
     id: 'UA-74645070-2',
     disabled: false,
+    debug: {
+      enabled: true, // default value
+      trace: false, // default value
+      sendHitTask: true // default value
+    }
   })
 } else if (typeof getGDPR !== 'undefined' && getGDPR === 'false') {
+  console.log('I am disabled')
   Vue.use(vueAnalytics, {
     id: 'UA-74645070-2',
     disabled: true,
