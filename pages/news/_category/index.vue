8ee31c35c1c11a7198c3404718f5f85d7c52e3e6
Andrew <a@fandb.pro>
2020-05-07 22:43:28 +0300

    test VI

--- pages/news/_category/index.vue
+++ pages/news/_category/index.vue
@@ -72,7 +72,7 @@
         w[n].push(function() {
             Ya.Context.AdvManager.render({
                 blockId: "R-A-568097-2",
-                renderTo: "yandex_rtb_R-A-568097-2",
+                renderTo: "yandex_rtb_${post}",
                 async: true,
                 pageNumber: ${post}
             });
